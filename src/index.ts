fc2ffc9c596082ade4db5e6c0c68088a81513542
mohamedaminetebini <tebbinimohamedamine@gmail.com>
2023-12-28 17:46:01 +0100

    fixed some typing

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,12 @@ import express from "express";
 const app:Express = express();
 app.use(express.json())
 const PORT = process.env.PORT || 3000;
-const USERS = [
+
+type User = {
+    username : string
+    password : string
+}
+const USERS : User[] = [
     {
         "username" : "amine",
         "password" : "tebini"
@@ -17,7 +22,7 @@ const USERS = [
 
 app.post('/sign-up',async(req:Request, res:Response) => {
     const {username, password} = req.body   
-    const filtred = USERS.filter(user => user.username === username )
+    const filtred : User[]  = USERS.filter(user => user.username === username )
     if(filtred.length > 0){
         res.status(409).send({message :"username is already taken"})
     } else if(password.length < 6){
@@ -29,8 +34,8 @@ app.post('/sign-up',async(req:Request, res:Response) => {
 } )
 
 app.post("/sign-in",async(req:Request, res:Response) => { 
-    const {username, password} = req.body
-    const filtred = USERS.filter(user => user.username === username  )
+    const {username, password}: User = req.body
+    const filtred : User[] = USERS.filter(user => user.username === username  )
     if(filtred.length === 0){
         res.status(404).send({message : "user does not exist"})
     } else if(filtred[0].password !== password){ 
